875b2136293c026eba895baa567541db07a9efd1
Ali-boubaker <alibenboubaker111@gmail.com>
2022-01-11 18:40:04 +0100

    login

--- Pets-mobile/App.js
+++ Pets-mobile/App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './login';
 import SignUp from './signup';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <SignUp/>
-      
+      <Login/>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
